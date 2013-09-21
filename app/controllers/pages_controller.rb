c8d6e487729b0c328ba32bcbb461b357de15726b
Salim Hbeiliny <salim@railslove.com>
2013-09-21 15:56:52 +0200

    create div with percentage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
     @politicians = Politican.where(electoral_district_id: @district)
     @bundesttag_elections = BundestagElection.all.map do |election_day|
       {
-        date: election_day.elective_date,
+        year: election_day.elective_date.year,
         numbers: election_day.bundestag_election_party_votes.map(&:party).map(&:short_name).zip(election_day.bundestag_election_party_votes.map(&:vote_percent))
       }
     end
